53697aa46ad7f8ec56119139f15514c34e11a3e1
antimatter15 <antimatter15@b1b77172-b5e1-11dd-a80a-757f205f25d6>
2008-11-19 02:44:17 +0000

    added test fixed generator

---
+++ lib.js
@@ -0,0 +1,22 @@
+//standard library for compilier
+
+function include(src){
+  return function(callback){
+    var s = document.createElement("script");
+    s.src = src;
+    s.type = "text/javascript";
+    s.onload = callback;
+    document.getElementsByTagName("HEAD")[0].appendChild(s);
+  }
+}
+
+function ajaxload(url){
+  return function(callback){
+    var x=new(this.ActiveXObject?ActiveXObject:XMLHttpRequest)('Microsoft.XMLHTTP');
+    x.open('GET', url, 1);
+    x.onreadystatechange=function(){
+      x.readyState>3&&callback(x.responseText,x):0
+    };
+    x.send(d)
+  }
+}
