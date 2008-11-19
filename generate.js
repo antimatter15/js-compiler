8ab5dc073bf7df75c20e2106f6fcf36ef5c4cdc3
antimatter15 <antimatter15@b1b77172-b5e1-11dd-a80a-757f205f25d6>
2008-11-19 02:33:19 +0000

    initial commits!

---
+++ generate.js
@@ -0,0 +1,36 @@
+//js++
+//a compiler for javascript that makes asynchronousity easy
+//100% awesome
+
+function compile(str){ //does not work
+  var i = 0;
+  var fn = "include";
+  while(i != -1){
+    i = str.indexOf("include(", i);
+    str = str.substr(0,i)+compilefn(str.substr(i), fn);
+  }
+  return str;
+  
+}
+
+function compilefn(str, fn){  
+  var fdex = str.indexOf(fn+"(")
+  
+  var edex = find(str.substr(fdex)) + 1;
+  
+  var startend = str.substr(fdex, edex);
+  
+  var outvar = fn + "_" + Math.floor(Math.random()*999999+99999);
+  
+  var ldex = str.substr(0, fdex).length - str.substr(0,fdex).split("").reverse().join("").indexOf(";")
+  
+  var start = str.substr(0, fdex).substr(0, ldex);
+  
+  var code = str.substr(0,fdex).substr(ldex);
+  
+  
+  var newstr = start+startend+"(function("+outvar+"){"+code+outvar+str.substr(fdex+edex)+"})"
+  
+  console.log(newstr)
+  
+}
